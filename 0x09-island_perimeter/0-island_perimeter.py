da7c7b6f13913d059645c028d7fcca6bcc66a0e9
Doreen Ikilai <dornkaizen@gmail.com>
2024-07-30 09:33:12 +0000

    refactor: Improve island_perimeter function

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,6 +6,7 @@ def island_perimeter(grid):
         return 0
 
     def is_water_or_out_of_bounds(x, y):
+        '''Check if the cell is water or out of bounds'''
         if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[0]):
             return True
         return grid[x][y] == 0
@@ -16,15 +17,15 @@ def island_perimeter(grid):
 
     for i in range(rows):
         for j in range(cols):
-            if grid[i][j] == 1:  # land cell
-                # Check all four directions
-                if is_water_or_out_of_bounds(i - 1, j):  # up
+            if grid[i][j] == 1:
+                '''Check all four directions'''
+                if is_water_or_out_of_bounds(i - 1, j):
                     perimeter += 1
-                if is_water_or_out_of_bounds(i + 1, j):  # down
+                if is_water_or_out_of_bounds(i + 1, j):
                     perimeter += 1
-                if is_water_or_out_of_bounds(i, j - 1):  # left
+                if is_water_or_out_of_bounds(i, j - 1):
                     perimeter += 1
-                if is_water_or_out_of_bounds(i, j + 1):  # right
+                if is_water_or_out_of_bounds(i, j + 1):
                     perimeter += 1
 
     return perimeter
